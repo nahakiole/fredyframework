a516691d0627cedffefd0d1f15d7bfa012b0412a
Nahakiole <robin.glauser@gmail.com>
2014-03-26 08:17:15 +0100

    Added datattypes.

---
+++ Model/Entity/Datatype/EMail.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 25.03.14
+ * Time: 16:38
+ */
+
+namespace Model\Entity\DataType;
+
+
+class EMail {
+
+} 
---
+++ Model/Entity/Datatype/Text.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 25.03.14
+ * Time: 16:36
+ */
+
+namespace Model\Entity\DataType;
+
+
+class Text {
+
+} 
