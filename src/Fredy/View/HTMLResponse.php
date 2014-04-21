0f450dd2e10ee9df1b76df084c615840897c492d
Nahakiole <robin.glauser@gmail.com>
2014-04-21 16:15:18 +0200

    Added css and js minifier.

--- src/Fredy/View/HTMLResponse.php
+++ src/Fredy/View/HTMLResponse.php
@@ -9,7 +9,8 @@
 
 namespace Fredy\View;
 
-
+use CSSmin;
+use JSMin;
 use Twig_SimpleFilter;
 
 class HTMLResponse extends TwigResponse
@@ -21,14 +22,52 @@ class HTMLResponse extends TwigResponse
         parent::__construct($templatePath);
     }
 
-    protected function addFilter() {
+    protected function addFilter()
+    {
+        $jsFilter = new Twig_SimpleFilter('minifyjs', array($this, 'minifyjs'));
+        $this->twig->addFilter($jsFilter);
 
-        $filter = new Twig_SimpleFilter('minifyjs', array($this, 'minifyjs'));
-        $this->twig->addFilter($filter);
+        $cssFilter = new Twig_SimpleFilter('minifycss', array($this, 'minifycss'));
+        $this->twig->addFilter($cssFilter);
     }
 
-    function minifyjs($url){
-        return strtoupper($url);
+    function minifyjs($url)
+    {
+        $file = ROOTPATH . $url;
+        if (file_exists($file)) {
+            $fileExtension = pathinfo($file, PATHINFO_EXTENSION);
+            $fileName = pathinfo($file, PATHINFO_FILENAME);
+            $fileFullName = pathinfo($file, PATHINFO_BASENAME);
+            $minifiedFileName = substr($url, 0, -strlen($fileFullName)) . $fileName . '.min.' . $fileExtension;
+            $changeDate = filectime($file);
+            $changeDateMinified = file_exists(ROOTPATH.$minifiedFileName) ? filectime(ROOTPATH.$minifiedFileName) : 0;
+            if ($changeDate < $changeDateMinified){
+                $fileContent = file_get_contents($file);
+                file_put_contents(ROOTPATH.$minifiedFileName, JSMin::minify($fileContent));
+            }
+            return $minifiedFileName;
+        }
+        return '';
+    }
+
+    function minifycss($url)
+    {
+        $file = ROOTPATH . $url;
+        if (file_exists($file)) {
+            $fileExtension = pathinfo($file, PATHINFO_EXTENSION);
+            $fileName = pathinfo($file, PATHINFO_FILENAME);
+            $fileFullName = pathinfo($file, PATHINFO_BASENAME);
+            $minifiedFileName = substr($url, 0, -strlen($fileFullName)) . $fileName . '.min.' . $fileExtension;
+            $changeDate = filectime($file);
+            $changeDateMinified = file_exists(ROOTPATH.$minifiedFileName) ? filectime(ROOTPATH.$minifiedFileName) : 0;
+            if ($changeDateMinified == 0 || $changeDate < $changeDateMinified){
+                $fileContent = file_get_contents($file);
+                $cssmin = new CSSmin();
+                file_put_contents(ROOTPATH.$minifiedFileName, $cssmin->run($fileContent));
+            }
+            return $minifiedFileName;
+        }
+        return '';
     }
 
     function render()
