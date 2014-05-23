f6fc5d9e64f995baca075218b34e7a86b648885b
Nahakiole <robin.glauser@gmail.com>
2014-05-24 00:48:58 +0200

    Added development and production modus.

--- example/index.php
+++ example/index.php
@@ -1,5 +1,6 @@
 <?php
 define("ROOTPATH", dirname(__FILE__) . '/');
 define("OFFSETPATH", '');
+define("MODE", 'prod');
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/nahakiole/fredyframework/src/Fredy/Run.php';
--- src/Fredy/Run.php
+++ src/Fredy/Run.php
@@ -1,7 +1,12 @@
 <?php
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
+if (MODE == 'dev'){
+    $whoops = new \Whoops\Run;
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->register();
+}
+elseif (MODE == 'prod'){
+    error_reporting(0);
+}
 $configuration = require_once ROOTPATH . "/Configuration.php";
 $services = require_once ROOTPATH . '/Services.php';
 new \Fredy\FredyAutoloader();
