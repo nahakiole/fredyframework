9d701b99c63d6735820313b16b9e7aeb79303159
Nahakiole <robin.glauser@gmail.com>
2014-05-24 00:13:38 +0200

    Cleaned up minifying code

--- src/Fredy/View/HTMLResponse.php
+++ src/Fredy/View/HTMLResponse.php
@@ -13,8 +13,7 @@ use CSSmin;
 use JSMin;
 use Twig_SimpleFilter;
 
-class HTMLResponse extends TwigResponse
-{
+class HTMLResponse extends TwigResponse {
 
 
     public function __construct($templatePath)
@@ -33,24 +32,35 @@ class HTMLResponse extends TwigResponse
 
     function minifyjs($url)
     {
-        $file = ROOTPATH . $url;
-        if (file_exists($file)) {
-            $fileExtension = pathinfo($file, PATHINFO_EXTENSION);
-            $fileName = pathinfo($file, PATHINFO_FILENAME);
-            $fileFullName = pathinfo($file, PATHINFO_BASENAME);
-            $minifiedFileName = substr($url, 0, -strlen($fileFullName)) . $fileName . '.min.' . $fileExtension;
-            $changeDate = filectime($file);
-            $changeDateMinified = file_exists(ROOTPATH.$minifiedFileName) ? filectime(ROOTPATH.$minifiedFileName) : 0;
-            if ($changeDateMinified == 0 || $changeDate < $changeDateMinified){
-                $fileContent = file_get_contents($file);
-                file_put_contents(ROOTPATH.$minifiedFileName, JSMin::minify($fileContent));
-            }
+        $minifiedFileName = $this->checkIfFileWasUpdated($url);
+        if ($minifiedFileName) {
+            $fileContent = file_get_contents(ROOTPATH . $url);
+            file_put_contents(ROOTPATH . $minifiedFileName, JSMin::minify($fileContent));
+
             return $minifiedFileName;
         }
-        return '';
+
+        return $url;
     }
 
     function minifycss($url)
+    {
+        $minifiedFileName = $this->checkIfFileWasUpdated($url);
+        if ($minifiedFileName) {
+            $fileContent = file_get_contents(ROOTPATH . $url);
+            $cssmin = new CSSmin();
+            file_put_contents(ROOTPATH . $minifiedFileName, $cssmin->run($fileContent));
+            return $minifiedFileName;
+        }
+        return $url;
+    }
+
+    /**
+     * @param $url
+     *
+     * @return bool
+     */
+    function checkIfFileWasUpdated($url)
     {
         $file = ROOTPATH . $url;
         if (file_exists($file)) {
@@ -59,15 +69,14 @@ class HTMLResponse extends TwigResponse
             $fileFullName = pathinfo($file, PATHINFO_BASENAME);
             $minifiedFileName = substr($url, 0, -strlen($fileFullName)) . $fileName . '.min.' . $fileExtension;
             $changeDate = filectime($file);
-            $changeDateMinified = file_exists(ROOTPATH.$minifiedFileName) ? filectime(ROOTPATH.$minifiedFileName) : 0;
-            if ($changeDateMinified == 0 || $changeDate < $changeDateMinified){
-                $fileContent = file_get_contents($file);
-                $cssmin = new CSSmin();
-                file_put_contents(ROOTPATH.$minifiedFileName, $cssmin->run($fileContent));
+            $changeDateMinified = file_exists(ROOTPATH . $minifiedFileName) ? filectime(ROOTPATH . $minifiedFileName)
+                : 0;
+            if ($changeDateMinified == 0 || $changeDate < $changeDateMinified) {
+                return $minifiedFileName;
             }
-            return $minifiedFileName;
         }
-        return '';
+
+        return null;
     }
 
     function render()
