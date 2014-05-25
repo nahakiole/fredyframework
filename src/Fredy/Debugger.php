f8fa8f6316fde364a15f96e2a482ec66edcac9f7
Nahakiole <robin.glauser@gmail.com>
2014-05-25 15:08:25 +0200

    Added Debugger and cleaned Run.php.

---
+++ src/Fredy/Debugger.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 25.05.14
+ * Time: 14:36
+ */
+
+namespace Fredy;
+
+
+class Debugger {
+
+    private static $whoops;
+    private static $fileHandler;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @param $mode
+     * Set the mode of the framework. This is either dev or prod at the moment.
+     * dev enables the errorhandler to show a fancy stacktrace
+     * prod removes all error output and shows no errors
+     */
+    public static function setMode($mode)
+    {
+        if ($mode == 'dev') {
+            self::$whoops = new \Whoops\Run;
+            self::$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+            self::$whoops->register();
+        } elseif ($mode == 'prod') {
+            ini_set('display_errors', 'Off');
+            error_reporting(0);
+        }
+    }
+
+    /**
+     * Enables output to file.
+     */
+    public static function enableFileOutput()
+    {
+        self::$fileHandler = fopen('log', 'a');
+    }
+
+    public static function log($string)
+    {
+        if (self::$fileHandler) {
+            fwrite(self::$fileHandler, date('l jS \of F Y h:i:s ') . " " . $string);
+        }
+        self::showMessage($string);
+    }
+
+    private static function showMessage($message)
+    {
+        var_dump($message);
+        echo "<br/>";
+    }
+
+
+}
